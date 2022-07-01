5bd8d1965ebe1cd1381fd6b154c034c2d3e71f1c
Ken <kingod0626@gmail.com>
2022-07-01 16:09:39 +0800

    新增檔案

---
+++ LoseOnlyPSRsimulator.py
@@ -0,0 +1,63 @@
+
+
+s = ''' 
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+r = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+p = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+storage = [p,s,r]
+player = input("輸入你出了啥s(剪刀)/r(石頭)/p(布)")
+
+
+if player == "s":
+    cpu = r
+    print(f"你出{s}")
+    print(f"電腦出{cpu}")
+    if cpu == s:
+        print("平手")
+    elif cpu == r:
+        print("你輸了")
+    elif cpu == p:
+        print("你贏了")
+elif player == "r":
+    cpu = p
+    print(f"你出{r}")
+    print(f"電腦出{cpu}")
+    if cpu == s:
+        print("你贏了")
+    elif cpu == r:
+        print("平手")
+    elif cpu == p:
+        print("你輸了")
+
+elif player == "p":
+    cpu = s
+    print(f"你出{p}")
+    print(f"電腦出{cpu}")
+    if cpu == s:
+        print("你輸了")
+    elif cpu == r:
+        print("你贏了")
+    elif cpu == p:
+        print("平手")
